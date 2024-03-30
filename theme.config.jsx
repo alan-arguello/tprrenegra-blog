9b0a4c8964c2e5dcdcddf236dd1e17117ac5a83f
alan-arguello <alan@jointrendi.com>
2024-03-30 16:53:12 -0600

    add frameworks

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,5 @@
 export default {
-      logo: <span>AI Architecture in Marvel's Spider-Man - made with Lumentis</span>,
+      logo: <span>Torrenegra Accelerator Frameworks</span>,
       editLink: {
         component: null,
       },
@@ -11,8 +11,8 @@ export default {
       },
       head: (
         <>
-          <meta property="og:title" content="AI Architecture in Marvel's Spider-Man" />
-          <meta property="og:description" content="The PrimarySource is a transcript of a presentation given by Adam Newnchester, a lead gameplay programmer at Insomniac Games, about the AI development process for the video game Marvel's Spider-Man. He covers topics such as behavior authoring, synced animations, combat iteration, procedural animation techniques, and challenges faced during development. The presentation is likely intended for an audience of game developers interested in AI programming and the development process of a successful video game." />
+          <meta property="og:title" content="Torrenegra Accelerator Frameworks" />
+          <meta property="og:description" content="Framworks para crecer tu compañía" />
           <meta name="robots" content="noindex, nofollow" />
         </>
       ),
