4714e262c88deeda8734ddb72bec626396c35cd3
alan-arguello <alan@jointrendi.com>
2024-03-30 12:15:21 -0600

    fix

--- pages/canales/tsup.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/app.ts"],
-  publicDir: false,
-  clean: true,
-  minify: true,
-  format: ["cjs", "esm"] // 👈 Node
-});
